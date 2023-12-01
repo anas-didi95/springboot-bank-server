38a9dcb4a3c6d2ccdfc70400550585e50eaf73de
Anas Juwaidi <anas.didi95@gmail.com>
2023-12-01 12:28:15 +0800

    REF: Refactor code

--- src/main/java/com/anasdidi/bank/domain/customer/Customer.java
+++ src/main/java/com/anasdidi/bank/domain/customer/Customer.java
@@ -1,38 +1,30 @@
 package com.anasdidi.bank.domain.customer;
 
-import java.time.LocalDateTime;
-
 import org.hibernate.annotations.GenericGenerator;
-import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import com.anasdidi.bank.common.BaseEntity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import jakarta.persistence.Temporal;
-import jakarta.persistence.TemporalType;
-import jakarta.persistence.Version;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import lombok.experimental.SuperBuilder;
 
 @Entity
-@EntityListeners(AuditingEntityListener.class)
 @Table(name = "t_cust")
 @Data
+@EqualsAndHashCode(callSuper = false)
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
-@Builder
-public class Customer {
+@SuperBuilder
+public class Customer extends BaseEntity {
 
   @Id
   @GenericGenerator(name = "generator", strategy = "guid")
@@ -45,26 +37,4 @@ public class Customer {
 
   @Column(name = "name", length = 200, nullable = false)
   private String name;
-
-  @CreatedBy
-  @Column(name = "created_by", length = 20, nullable = false)
-  private String createdBy;
-
-  @CreatedDate
-  @Column(name = "created_dt", nullable = false)
-  @Temporal(TemporalType.TIMESTAMP)
-  private LocalDateTime createdDate;
-
-  @LastModifiedBy
-  @Column(name = "updated_by", length = 20, nullable = false)
-  private String updatedBy;
-
-  @LastModifiedDate
-  @Column(name = "updated_dt", nullable = false)
-  @Temporal(TemporalType.TIMESTAMP)
-  private LocalDateTime updatedDate;
-
-  @Version
-  @Column(name = "version", nullable = false)
-  private Integer version;
 }
