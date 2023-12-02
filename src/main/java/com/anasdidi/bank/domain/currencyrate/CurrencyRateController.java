3add3028110576e7d1fcac3eb2483a340ad0112c
Anas Juwaidi <anas.didi95@gmail.com>
2023-12-02 15:06:13 +0800

    MOD: Amend handler to get currency rate list

--- src/main/java/com/anasdidi/bank/domain/currencyrate/CurrencyRateController.java
+++ src/main/java/com/anasdidi/bank/domain/currencyrate/CurrencyRateController.java
@@ -1,9 +1,13 @@
 package com.anasdidi.bank.domain.currencyrate;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
@@ -16,10 +20,11 @@ public class CurrencyRateController {
   private final CurrencyRateService currencyRateService;
 
   @GetMapping("/{fromCurrency}/{toCurrency}")
-  ResponseEntity<CurrencyRateDTO> getCurrencyRate(
+  ResponseEntity<List<CurrencyRateDTO>> getCurrencyRateList(
       @PathVariable String fromCurrency,
-      @PathVariable String toCurrency) {
-    CurrencyRateDTO responseBody = currencyRateService.getCurrencyRate(fromCurrency, toCurrency);
+      @PathVariable String toCurrency,
+      @RequestParam(required = false, name = "date") List<LocalDate> dateList) {
+    List<CurrencyRateDTO> responseBody = currencyRateService.getCurrencyRateList(fromCurrency, toCurrency, dateList);
     return ResponseEntity.ok().body(responseBody);
   }
 }
