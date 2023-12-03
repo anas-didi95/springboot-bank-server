6d65fe957d6d25294be4dc4ade3cdad2557d2d9f
Anas Juwaidi <anas.didi95@gmail.com>
2023-12-03 21:46:27 +0800

    ADD: Add log filter

---
+++ src/main/java/com/anasdidi/bank/filter/RequestResponseLogFilter.java
@@ -0,0 +1,73 @@
+package com.anasdidi.bank.filter;
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.function.Function;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.GenericFilterBean;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+import org.springframework.web.util.ContentCachingResponseWrapper;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@Slf4j
+public class RequestResponseLogFilter extends GenericFilterBean {
+
+  @Override
+  public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain)
+      throws IOException, ServletException {
+    ContentCachingRequestWrapper requestWrapper = requestWrapper(request);
+    ContentCachingResponseWrapper responseWrapper = responseWrapper(response);
+
+    filterChain.doFilter(requestWrapper, responseWrapper);
+    logRequest(requestWrapper);
+    logResponse(responseWrapper);
+  }
+
+  private ContentCachingRequestWrapper requestWrapper(ServletRequest request) {
+    if (request instanceof ContentCachingRequestWrapper requestWrapper) {
+      return requestWrapper;
+    }
+    return new ContentCachingRequestWrapper((HttpServletRequest) request);
+  }
+
+  private ContentCachingResponseWrapper responseWrapper(ServletResponse response) {
+    if (response instanceof ContentCachingResponseWrapper responseWrapper) {
+      return responseWrapper;
+    }
+    return new ContentCachingResponseWrapper((HttpServletResponse) response);
+  }
+
+  private void logRequest(ContentCachingRequestWrapper request) {
+    StringBuilder builder = new StringBuilder();
+    builder.append(headersToString(Collections.list(request.getHeaderNames()), request::getHeader));
+    builder.append(new String(request.getContentAsByteArray()));
+    log.info("Request: {}", builder);
+  }
+
+  private void logResponse(ContentCachingResponseWrapper response) throws IOException {
+    StringBuilder builder = new StringBuilder();
+    builder.append(headersToString(response.getHeaderNames(), response::getHeader));
+    builder.append(new String(response.getContentAsByteArray()));
+    log.info("Response: {}", builder);
+    response.copyBodyToResponse();
+  }
+
+  private String headersToString(Collection<String> headerNames, Function<String, String> headerValueResolver) {
+    StringBuilder builder = new StringBuilder();
+    for (String headerName : headerNames) {
+      String header = headerValueResolver.apply(headerName);
+      builder.append("%s=%s".formatted(headerName, header)).append("\n");
+    }
+    return builder.toString();
+  }
+}
